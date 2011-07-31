46119ff39b6e4c5da80f2ef680dc665823dfa852
Jacob Silterra <jsilter@gmail.com>
2011-07-31 15:20:42 -0400

    Add wound healing test, make it optional with flag (takes 15 seconds).
    Add tst_manual, for testing h5 files we make manually.

--- cellprofiler/tests/test_multiprocess.py
+++ cellprofiler/tests/test_multiprocess.py
@@ -5,14 +5,14 @@ Created on Jul 23, 2011
 '''
 import unittest
 import os
-import time
 
 from cellprofiler.pipeline import Pipeline
-import cellprofiler.distributed as cpdistributed
 import cellprofiler.multiprocess_server as multiprocess_server
 import cellprofiler.measurements as measurements
 
 from test_Measurements import compare_measurements
+
+RUN_EXTENDED = False
 
 
 class TestMultiProcess(unittest.TestCase):
@@ -27,39 +27,37 @@ class TestMultiProcess(unittest.TestCase):
         self.datadir = None
         self.port = None
 
-    def run_pipeline_multi(self,pipeline,output_file_path,status_callback):
-        distributor = cpdistributed.Distributor(None)
-        distributor.start_serving(pipeline,self.port,output_file_path, status_callback)
-        print "serving at ", distributor.server_URL
-        #Start workers
-        donejobs = multiprocess_server.run_multiple_workers(distributor.server_URL)
-        
-        running_pipeline = distributor.run_with_yield()
-        for ghost in running_pipeline:
-            time.sleep(0.1)
-            pass
-            #tm = time.time()
-            #print "%s \t %s" % (tm,ghost)
-      
     def tst_pipeline_multi(self,pipeline_path,ref_data_path,output_file_path):
         pipeline= Pipeline()
         pipeline.load(pipeline_path)
         
-        self.run_pipeline_multi(pipeline,output_file_path,None)
+        multiprocess_server.run_pipeline_multi(pipeline,self.port,output_file_path,None)
         
         ref_meas = measurements.load_measurements(ref_data_path)
         test_meas = measurements.load_measurements(output_file_path)
 
         compare_measurements(ref_meas, test_meas,check_feature)
         
+    @unittest.skipIf(not RUN_EXTENDED,
+                     "Lengthy test, skipping to save time")
     def test_wound_healing(self):
-        pipeline_path = os.path.join(self.datadir,'ExampleWoundHealingImages/ExampleWoundHealing.cp')
+        pipeline_path = os.path.join(self.datadir,'ExampleWoundHealingImages','ExampleWoundHealing.cp')
         ref_data_path = os.path.join(self.datadir,'ExampleWoundHealingImages','ExampleWoundHealing_ref.h5')
-
         output_file_path = os.path.join(self.datadir,'output/test_wound_healing.h5')
         
         self.tst_pipeline_multi(pipeline_path,ref_data_path,output_file_path)
+        
+    def tst_manual(self):
+        ex_path = os.path.join(self.datadir,'ExampleTrackObjects')
+        ref_data_path = os.path.join(ex_path,'ExampleTrackObjects_norm_single.h5')
+        
+        norm_multi_path = os.path.join(ex_path,'ExampleTrackObjects_norm_multi.h5')
+        dist_single_path = os.path.join(ex_path,'ExampleTrackObjects_dist_single.h5')
 
+        to_check = [norm_multi_path]#,dist_single_path]
+        for path in to_check:
+            compare_measurements(ref_data_path,path,check_feature)
+        
 def check_feature(feat_name):
         fnl = feat_name.lower()
         ignore = ['executiontime','pathname','filename']
